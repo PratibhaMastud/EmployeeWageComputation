03cabf70fd650218900b9fc687d4c400ad0fd4e8
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-04 23:58:43 +0530

    [Pratibha Mastud] Add: Check Employee Present or Not

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome Employee Wage Computation");
+			checkEmployeeAttendence();
+		}
+
+		public static void checkEmployeeAttendence()
+		{
+			Random rd = new Random();
+			int rand_num = rd.Next(0, 1);
+			if (rand_num == 1)
+				Console.WriteLine("Employee is Present");
+			else
+				Console.WriteLine("Employee is Not Present");
+		}
+	}
+}
