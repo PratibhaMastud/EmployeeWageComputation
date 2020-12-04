d9a3dca91ab545f030aa88badfbecd3f63a97b39
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-05 00:40:03 +0530

    [Pratibha Mastud] Add: Find employee wage using switch case

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -4,17 +4,30 @@ namespace EmployeeWage
 {
     class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            partTimeEmployeeWage();
-        }
-        public static int partTimeEmployeeWage()
-        {
-            int wagePerHour = 20;
-            int dayHour = 4;
-            int dailyEmployeeWage = wagePerHour * dayHour;
-            Console.WriteLine("Part time Employee Wage is : {0}", dailyEmployeeWage);
-            return dailyEmployeeWage;
+            Console.WriteLine("Welcom to Employee Wage");
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage : " + empWage);
         }
     }
 }
