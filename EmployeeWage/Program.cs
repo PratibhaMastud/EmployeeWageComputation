1d402441e82f26ba3b45f443c43866b59beb5f25
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-05 00:52:36 +0530

    [Pratibha Mastud] Add: Calculate wage till working days in month

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -8,12 +8,14 @@ namespace EmployeeWage
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HRS_IN_MONTH = 10;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcom to Employee Wage Calculation Project !");
+            Console.WriteLine("Welcom to Employee Wage");
             //Variables
-            int empHrs = 0, empWage = 0, totalEmpWage = 0;
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
                 Random random = new Random();
                 //Computation
@@ -30,10 +32,11 @@ namespace EmployeeWage
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                Console.WriteLine("Emp Wage : " + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day : " + totalWorkingDays + "Emp Hrs : " + empHrs);
             }
-            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Employee Wage : " + totalEmpWage);
         }
     }
 }
