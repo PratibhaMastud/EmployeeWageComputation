c72cd332b9b61f5e2fa34367656f9fa6f5e16986
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-05 01:20:57 +0530

    [Pratibha Mastud] Add: Compute Employee Wage By using Separate Class

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -4,45 +4,17 @@ namespace EmployeeWage
 {
     class Program
     {
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HRS_IN_MONTH = 10;
-
-        public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-        {
-            
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-            {
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day : " + totalWorkingDays + "Emp Hrs : " + empHrs);
-            }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Employee Wage : " + totalEmpWage);
-            return totalEmpWage;
-        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcom to Employee Wage");
-            computeEmpWage("DMart", 20, 4, 10);
-            computeEmpWage("Relience", 10, 4, 20);
-        }
+            EmpBuilderClass dMart = new EmpBuilderClass("DMart", 20, 2, 10);
+            EmpBuilderClass relience = new EmpBuilderClass("Relience", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            relience.computeEmpWage();
+            Console.WriteLine(relience.toString());
 
+
+        }
     }
 }
